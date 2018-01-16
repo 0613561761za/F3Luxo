42655eafc54377bc260697f52065943957c333f4
EraHitam <35055598+EraHitam@users.noreply.github.com>
2018-01-16 23:36:15 +0800

    Update benchmark.sh

--- For7_only/benchmark.sh
+++ For7_only/benchmark.sh
@@ -3,4 +3,4 @@
 # go to root
 cd
 
-wget https://raw.githubusercontent.com/Mbah-Shondong/Debian732/master/Debian7/bench.sh -O - -o /dev/null|bash
+wget https://raw.githubusercontent.com/EraHitam/F3Luxo/master/For7_only/bench.sh -O - -o /dev/null|bash
